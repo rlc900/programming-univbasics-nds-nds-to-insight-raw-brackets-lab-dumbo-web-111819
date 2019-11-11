697a7df8a7760868baf0c2385ce7f96c1db15b90
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-11 02:17:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -16,7 +16,7 @@ def directors_totals(nds)
     result[director_name] = 0 
     movie_index = 0 
     while movie_index < nds[director_index][:movies].length do 
-      result[director_name] = nds[director_index][:movies][movie_index][:worldwide_gross]
+      result[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
       movie_index += 1 
     end 
     director_index += 1 
